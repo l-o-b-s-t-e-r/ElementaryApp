2a15c8aa5c44286d1cdbc3058631bf34aba9102d
Vlad <vladislav-loboda@mail.ru>
2017-10-07 18:03:44 +0300

    map

--- app/src/main/java/loboda/elementary/app/ui/map/MapFragment.java
+++ app/src/main/java/loboda/elementary/app/ui/map/MapFragment.java
@@ -1,13 +1,35 @@
 package loboda.elementary.app.ui.map;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import loboda.elementary.app.App;
 import loboda.elementary.app.R;
 import loboda.elementary.app.databinding.FragmentMapBinding;
 import loboda.elementary.app.di.modules.MapModule;
 import loboda.elementary.app.ui.base.BaseFragment;
 
-public class MapFragment extends BaseFragment<MapPresenter, FragmentMapBinding> implements IMapPresenter.View {
+public class MapFragment extends BaseFragment<MapPresenter, FragmentMapBinding> implements IMapPresenter.View, OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        ((SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map))
+                .getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
 
     @Override
     public void inject() {
